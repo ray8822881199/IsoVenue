73c62ac97b10c5b3eff270a5f5eeae23ded675ce
ray8822881199 <ray8822881199@gmail.com>
2025-12-07 23:43:27 +0800

    fix: 調整了許多甲方的細節

--- main.js
+++ main.js
@@ -541,7 +541,7 @@ function loadStoreData($element) {
             for(var index in infoObj.sns_link?.split(',')){
                 const snslink = infoObj.sns_link?.split(',')[index];
                 const snstype = infoObj.sns_type?.split(',')[index];
-                const $sns_link = $(`<a href="${snslink}" target="_blank">${snstype}</a><br>`);
+                const $sns_link = $(`<a href="${snslink}" target="_blank">${snstype} <svg class="link_svg" fill="currentColor" width="20px" height="20px" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z"></path></svg></a><br>`);
                 $('.stall_sns_link').append($sns_link);
             }
             //const $sns_link = $(`<a href="${infoObj.sns_link||''}" target="_blank">${infoObj.sns_type||''}</a>`);
